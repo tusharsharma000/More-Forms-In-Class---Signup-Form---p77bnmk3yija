4bc5b05da1133cb3a664b2d3a4b3aa015b507096
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-02-27 10:53:44 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,70 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const inputfields = {
+    name: "",
+    email: "",
+    gender: "",
+    phone: "",
+    password: ""
+  };
+  const [formvalue, setFormValue] = useState(inputfields);
+  const handlechange = (e) => {
+    const { name, values } = e.target;
+    setFormValue({ ...formvalue, [name]: values });
+  };
 
+  return (
+    <div id="main">
+      Name:
+      <input
+        type="text"
+        data-testid="name"
+        name="name"
+        value={formvalue.name}
+        onChange={handlechange}
+      />
+      Email:
+      <input
+        type="email"
+        data-testid="email"
+        name="email"
+        value={formvalue.email}
+        onChange={handlechange}
+      />
+      Gender:
+      <select
+        data-testid="gender"
+        name="male"
+        value={formvalue.gender}
+        onChange={handlechange}
+      >
+        <option>Male</option>
+        <option>Female</option>
+        <option>Other</option>
+      </select>
+      Phone Number:
+      <input
+        type="number"
+        data-testid="phoneNumber"
+        name="phone"
+        value={formvalue.phone}
+        onChange={handlechange}
+      />
+      Password:
+      <input
+        type="password"
+        data-testid="password"
+        name="password"
+        value={formvalue.password}
+        onChange={handlechange}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
 export default App;
